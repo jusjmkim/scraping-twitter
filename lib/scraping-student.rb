4a904fbe4a2c287ab6fd6a8f0026414525f958db
Justin Kim <justinjmkim@gmail.com>
2014-06-13 11:32:38 -0400

    Add initialize, scrape_student_profiles, scrape_students, normalize methods

--- lib/scraping-student.rb
+++ lib/scraping-student.rb
@@ -1,38 +1,27 @@
 class ScrapingStudent
 
-  doc = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/'))
-  @students = []
-  student_profiles = doc.search("div.blog-thumb").css("a").collect{|link| link.attribute("href").value}.uniq
+  attr_accessor :index_url
+  attr_reader :student_profiles, :doc
 
-  #input array into hash
-  def array_into_hash(symbol, array, students)
-    if students.empty?
-      array.each do |element|
-        student_hash = {}
-        student_hash[symbol] = element
-        students << student_hash
-      end
-    else
-      students.each_with_index do |hash, index|
-      hash[symbol] = array[index]
-      end
-    end
-    students
+  def initialize(index_url)
+    @index_url = index_url
+    @doc = doc = Nokogiri::HTML(open(index_url))
+    scrape_student_profiles
   end
 
-  #names
-  names = doc.css("h3").text.split("\n").collect{|word|word.strip}.uniq
-  names.slice!(0)
-  names.slice!(1)
-  array_into_hash(:name, names, @students)
-
-  twitter = []
-  student_profiles.each do |profile|
-    page = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/#{profile}"))
-
-    tweet = page.search("div.social-icons").css("a").attribute("href").value
-    twitter << tweet
+  def scrape_student_profiles
+    @student_profiles ||= doc.search("div.blog-thumb").css("a").collect { |link|
+      normalize_student_profiles(link["href"])}.uniq
   end
-  array_into_hash(:twitter, twitter, @students)
+
+  def scrape_students
+    student_profiles.collect { |profile| Student.new(profile)}
+  end
+
+  private
+
+  def normalize_student_profiles(profile)
+    profile.start_with?("http://") ? profile : "#{index_url}#{profile}"
+  end
 
 end
