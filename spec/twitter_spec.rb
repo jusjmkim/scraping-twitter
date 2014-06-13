34714a7c7a3e9595d6eff4ff31526a7d3d3710bd
Amber Tunnell <amber.tunnell@gmail.com>
2014-06-13 14:24:29 -0400

    updates twitter rspec

--- spec/twitter_spec.rb
+++ spec/twitter_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 
 describe Twitter do
- let!(:twitter){Twitter.new(["Hi, I'm a tweet!","I'm also a tweet!","Hello, world!"])}
+ let(:twitter){Twitter.new(["Hi, I'm a tweet!","I'm also a tweet!","Hello, world!"])}
 
   describe ".all" do
     it 'keeps track of student tweets' do
@@ -15,14 +15,14 @@ describe Twitter do
     end
   end  
 
-  describe ".number_of_tweets" do
+  describe "#number_of_tweets" do
     it 'outputs the number of tweets' do
-      expect(twitter).to eq(3)
+      expect(twitter.number_of_tweets).to eq(3)
     end   
   end   
 
-  describe ".keyword_count" do
-    it 'outputs the number of times a keyword is included in a tweet'
+  describe "#keyword_count" do
+    it 'outputs the number of times a keyword is included in a tweet' do
       expect(twitter("also")).to eq(1) 
     end
   end   
