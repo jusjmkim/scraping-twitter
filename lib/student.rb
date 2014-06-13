1770a5cbf5cedac331e05c719c3cb8c70fa21f90
Justin Kim <justinjmkim@gmail.com>
2014-06-13 11:35:57 -0400

    Define all, initialize, name, and twitter methods

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,25 @@
 class Student
-  
+  attr_accessor :student_profile
+  attr_reader :doc
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(profile)
+    @profile = profile
+    @doc = Nokogiri::HTML(open(profile_url))
+    @@all << self
+  end
+
+  def name
+      @name ||= doc.search("div.page-title h4").text
+  end
+
+  def twitter
+    @twitter ||= doc.search("div.social-icons a[href*='twitter.com']").first["href"]
+  end 
+
 end
--- scraping-twitter.rb
+++
@@ -1,3 +0,0 @@
-class ScrapingTwitter
-  
-end  
