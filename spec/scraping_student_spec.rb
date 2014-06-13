dcc24210f9aecfba9214479b24f970674224a6cc
Chris Callahan <callahan.c@gmail.com>
2014-06-13 12:22:13 -0400

    add tests for ScrapingStudent class

--- spec/scraping_student_spec.rb
+++ spec/scraping_student_spec.rb
@@ -1,5 +1,24 @@
 require_relative './spec_helper'
 
 describe ScrapingStudent do
+  let(:scraper){ScrapingStudent.new("http://ruby005.students.flatironschool.com/")}
+
+  describe "#initialize" do
+    it "takes the index page as an argument" do
+      expect(scraper.index_url).to eq("http://ruby005.students.flatironschool.com/")
+    end
+  end
+
+  describe "#scrape_student_urls" do
+    it "should return an array of student profile urls" do
+      scraper.scrape_student_urls
+      expect(scraper.student_profiles).to include("http://ruby005.students.flatironschool.com/students/chriscallahan.html")
+    end
+  end
+
+  describe "#create_student_instances" do
+    scraper.create_student_instances
+    expect(scraper.create_student_instances.first).to be_an_instance_of(Student)
+  end  
 
 end
