4d11b07ba4a8724af8c515803833fcdb938d4fa9
Amber Tunnell <amber.tunnell@gmail.com>
2014-06-13 14:14:37 -0400

    adds rspec test for twitter.rb

--- spec/twitter_spec.rb
+++ spec/twitter_spec.rb
@@ -1,5 +1,29 @@
 require_relative './spec_helper'
 
 describe Twitter do
+ let!(:twitter){Twitter.new(["Hi, I'm a tweet!","I'm also a tweet!","Hello, world!"])}
 
-end
+  describe ".all" do
+    it 'keeps track of student tweets' do
+      expect(Twitter.all).to include(twitter)
+    end
+  end
+
+  describe "#initialize" do
+    it 'accepts the students\' tweets' do
+      expect(student_tweets).to eq(["Hi, I'm a tweet!","I'm also a tweet!","Hello, world!"])
+    end
+  end  
+
+  describe ".number_of_tweets" do
+    it 'outputs the number of tweets' do
+      expect(twitter).to eq(3)
+    end   
+  end   
+
+  describe ".keyword_count" do
+    it 'outputs the number of times a keyword is included in a tweet'
+      expect(twitter("also")).to eq(1) 
+    end
+  end   
+end   
