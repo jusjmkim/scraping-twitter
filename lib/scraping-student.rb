49c1c90e8584897d64ab5bec02a650fc21327d97
Justin Kim <justinjmkim@gmail.com>
2014-06-13 11:18:17 -0400

    Make student files classes

--- lib/scraping-student.rb
+++ lib/scraping-student.rb
@@ -1,38 +1,38 @@
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
+class ScrapingStudent
 
-doc = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/'))
-@students = []
-student_profiles = doc.search("div.blog-thumb").css("a").collect{|link| link.attribute("href").value}.uniq
+  doc = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/'))
+  @students = []
+  student_profiles = doc.search("div.blog-thumb").css("a").collect{|link| link.attribute("href").value}.uniq
 
-#input array into hash
-def array_into_hash(symbol, array, students)
-  if students.empty?
-    array.each do |element|
-      student_hash = {}
-      student_hash[symbol] = element
-      students << student_hash
+  #input array into hash
+  def array_into_hash(symbol, array, students)
+    if students.empty?
+      array.each do |element|
+        student_hash = {}
+        student_hash[symbol] = element
+        students << student_hash
+      end
+    else
+      students.each_with_index do |hash, index|
+      hash[symbol] = array[index]
+      end
     end
-  else
-    students.each_with_index do |hash, index|
-    hash[symbol] = array[index]
-    end
+    students
   end
-  students
+
+  #names
+  names = doc.css("h3").text.split("\n").collect{|word|word.strip}.uniq
+  names.slice!(0)
+  names.slice!(1)
+  array_into_hash(:name, names, @students)
+
+  twitter = []
+  student_profiles.each do |profile|
+    page = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/#{profile}"))
+
+    tweet = page.search("div.social-icons").css("a").attribute("href").value
+    twitter << tweet
+  end
+  array_into_hash(:twitter, twitter, @students)
+
 end
-
-#names
-names = doc.css("h3").text.split("\n").collect{|word|word.strip}.uniq
-names.slice!(0)
-names.slice!(1)
-array_into_hash(:name, names, @students)
-
-twitter = []
-student_profiles.each do |profile|
-  page = Nokogiri::HTML(open("http://ruby005.students.flatironschool.com/#{profile}"))
-
-  tweet = page.search("div.social-icons").css("a").attribute("href").value
-  twitter << tweet
-end
-array_into_hash(:twitter, twitter, @students)
