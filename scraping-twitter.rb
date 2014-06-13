bd7430d095e15e29f20dff838053b024ef501ac8
Amber Tunnell <amber.tunnell@gmail.com>
2014-06-13 12:02:01 -0400

    updates scraping twitter

--- scraping-twitter.rb
+++ scraping-twitter.rb
@@ -1,16 +1,14 @@
 class ScrapingTwitter
 
+  attr_reader :student_tweets 
+
   Student.all
 
   student_twitters = Student.all.collect{|student| student.twitter}
 
-  student_tweets = Array.new
-  student_twitters.each do |twitter_profile|
+  student_tweets = student_twitters.collect do |twitter_profile|
     page = Nokogiri::HTML(open("http://twitter.com/#{twitter_profile}"))
-    binding.pry 
     tweets = page.search("div.ProfileTweet-contents")
-    student_tweets << tweets 
   end   
-  array_into_hash(:tweets,student_tweets,@@students)
 
 end  
