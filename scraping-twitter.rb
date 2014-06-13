faa2f40a74ce357937c3568360e4acab8fcc7107
Amber Tunnell <amber.tunnell@gmail.com>
2014-06-13 12:04:11 -0400

    updates ScrapingTwitter class

--- scraping-twitter.rb
+++ scraping-twitter.rb
@@ -2,9 +2,9 @@ class ScrapingTwitter
 
   attr_reader :student_tweets 
 
-  Student.all
-
-  student_twitters = Student.all.collect{|student| student.twitter}
+  def initialize
+    @student_twitters = Student.all.collect{|student| student.twitter}
+  end
 
   student_tweets = student_twitters.collect do |twitter_profile|
     page = Nokogiri::HTML(open("http://twitter.com/#{twitter_profile}"))
